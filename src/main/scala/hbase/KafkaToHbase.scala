05308d3b311cec598eec622036a76e7e3ceae3ad
jb pettit <jbpettit@ebi.ac.uk>
2015-01-21 03:12:14 +0000

    Trying hbase connexion

--- src/main/scala/hbase/KafkaToHbase.scala
+++ src/main/scala/hbase/KafkaToHbase.scala
@@ -9,32 +9,45 @@ import org.apache.hadoop.hbase.client.HConnectionManager
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.hbase.util.Bytes
 
-
-
-
-
-
+/*Stores and reads values in the Hbase*/
 case class KafkaToHbase() {
 	
+  
+	/*Creating configuration and connecting*/
 	val conf = new Configuration()
 	val admin = new HBaseAdmin(conf)
 	
-	// list the tables
-	val listtables=admin.listTables() 
-	listtables.foreach(println)
+	/*Generic function to insert in HBase*/
+	def insert[T](table:String, rowkey:String, familly:String, column:String, value:T, convertToBytes:(T)=>Array[Byte]) {
+		/*Fetch the table*/
+		val httable = new HTable(conf, table)
+		
+		/*Defining the rowkey*/
+		val theput= new Put(Bytes.toBytes(rowkey))
+		
+		theput.add(Bytes.toBytes(familly),Bytes.toBytes(column),convertToBytes(value))
+		httable.put(theput)		
+	}
 	
-	// let's insert some data in 'mytable' and get the row
+	/*Is the keyrow/column already defined?*/
+	def exists(table:String, rowkey:String, familly:String, column:String): Boolean =  {
+		/*Fetch the table*/
+		val httable = new HTable(conf, table)	
+		val theget= new Get(Bytes.toBytes(rowkey))
+		
+		val result=httable.get(theget)
+		/*return boolean*/
+		!result.containsEmptyColumn(Bytes.toBytes(familly), Bytes.toBytes(column))		
+	}
 	
-	val table = new HTable(conf, "article_links")
-	
-	val theput= new Put(Bytes.toBytes("rowkey1"))
-	
-	theput.add(Bytes.toBytes("infos"),Bytes.toBytes("id1"),Bytes.toBytes("one"))
-	table.put(theput)
-	
-	val theget= new Get(Bytes.toBytes("rowkey1"))
-	val result=table.get(theget)
-	val value=result.value()
-	println(Bytes.toString(value))
+	/* 
+	 * Insert URL link in the article_links table
+	 * Parameters are the column the value
+	 */
+	def insertURL(hashURL:String,URL:String) {
+		if(!exists("article_links",hashURL,"infos","URL")) {
+			insert[String]("article_links",hashURL,"infos","URL",URL,s => Bytes.toBytes(s))
+		}
+	}
 }
 	
