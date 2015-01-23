741458387cca19375b60d42fd04b94871419a6c8
jb pettit <jbpettit@ebi.ac.uk>
2015-01-23 11:40:23 +0000

    Reading data directly from Hbase to Spark

--- src/main/scala/TopicsFinder.scala
+++ src/main/scala/TopicsFinder.scala
@@ -61,7 +61,7 @@ object TopicsFinder {
 		  .map(tuple => tuple._2)
 		  
 		  tokenized.foreach(r =>{
-		    println(new String(r.getRow()))
+		    println(new String(r.getColumnLatest("contents".getBytes(), "title".getBytes()).getValue()))
 		  })
 		  
 		/*val int = tokenized  
