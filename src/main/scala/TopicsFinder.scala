95c59a8a095a1ccb736f6bd0d969e7e3bd1afc3f
jb pettit <jbpettit@ebi.ac.uk>
2015-01-23 10:27:51 +0000

    Changing Kafka message format, implemented the API redaings from FB and Disqus

--- src/main/scala/TopicsFinder.scala
+++ src/main/scala/TopicsFinder.scala
@@ -11,6 +11,12 @@ import scala.collection.mutable.ArrayBuffer
 import org.apache.spark._
 import org.apache.spark.SparkContext._
 import main.scala.rss.ArticleMeta
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.hadoop.hbase.mapreduce.TableInputFormat
+import org.apache.spark.rdd.NewHadoopRDD
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable
+import org.apache.hadoop.hbase.client.Result
+
 
 object TopicsFinder {
   
@@ -19,7 +25,22 @@ object TopicsFinder {
 	
 	val conf = new SparkConf().setAppName("Spark Topics").setMaster("local")
     val spark = new SparkContext(conf)
-
+  	
+  	/*Creating HBase configuration*/
+	val hbaseConfiguration = (hbaseConfigFileName: String, tableName: String) => {
+	  val hbaseConfiguration = HBaseConfiguration.create()
+	  hbaseConfiguration.addResource(hbaseConfigFileName)
+	  hbaseConfiguration.set(TableInputFormat.INPUT_TABLE, tableName)
+	  hbaseConfiguration
+	 }
+	
+	val rdd = new NewHadoopRDD(
+	  spark,
+	  classOf[TableInputFormat],
+	  classOf[ImmutableBytesWritable],
+	  classOf[Result],
+	  hbaseConfiguration("/opt/cloudera/parcels/CDH-5.3.0-1.cdh5.3.0.p0.30/etc/hbase/conf.dist/", "article_links")
+	)
 	
 	val dictionnary = Source.fromFile("common_words") mkString
 	
@@ -32,9 +53,13 @@ object TopicsFinder {
 		/*Cleaning*/
 		val corpusStriped = all.replaceAll("[^a-zA-Z ]", "").toLowerCase()
 		
-		val tokenized = spark.parallelize(corpusStriped :: Nil).flatMap(_.split(" "))
+		//val tokenized = spark.parallelize(corpusStriped :: Nil).flatMap(_.split(" "))
 		
-		val wordCounts = tokenized.map((_, 1)).reduceByKey(_ + _)
+		val tokenized = rdd
+		  .map(tuple => tuple._2)
+		  .map(result => result.getColumnCells("contents".getBytes(), "description".getBytes()))
+		
+		val wordCounts = tokenized.map(cell => (new String(cell.get(0).getValue()), 1)).reduceByKey(_ + _)
 		
 		// filter out words with less than threshold occurrences
 		val filtered = wordCounts.filter((tuple) =>{
