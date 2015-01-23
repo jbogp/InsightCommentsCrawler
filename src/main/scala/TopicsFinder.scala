cf3a1f69a6e1b1513a25e65ca611f6c62f50fa53
jb pettit <jbpettit@ebi.ac.uk>
2015-01-23 12:40:34 +0000

    Reading data directly from Hbase to Spark

--- src/main/scala/TopicsFinder.scala
+++ src/main/scala/TopicsFinder.scala
@@ -70,7 +70,7 @@ object TopicsFinder {
 		  })
 		  
 		val wordCounts = test
-		  .flatMap(_.replaceAll("[^a-zA-Z ]", "").toLowerCase().split(" ").drop(1))  
+		  .flatMap(_.replaceAll("[^a-zA-Z ]", "").toLowerCase().split(" "))  
 		  .map((_,1)).reduceByKey(_ + _)
 		
 		
@@ -82,8 +82,10 @@ object TopicsFinder {
 		println(filtered
 		    .collect
 		    .sortBy(r=>r._2)
-		    .takeRight(10)
+		    .takeRight(11)
 		    .reverse
+		    //Removing empty string
+		    .drop(1)
 		    .reduceLeft((s,i) => ("("+s._1+","+s._2+"),("+ i._1+","+i._2+")",1)))
 		//tokenized.collect.foreach(res => println(new String(res)))
 		
