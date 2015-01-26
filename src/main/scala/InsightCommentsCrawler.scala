2b1928e2885f7f59725dd1ecfb229a7409150f75
jb pettit <jbpettit@ebi.ac.uk>
2015-01-26 06:26:08 +0000

    Registering link articles in comments HBase

--- src/main/scala/InsightCommentsCrawler.scala
+++ src/main/scala/InsightCommentsCrawler.scala
@@ -142,7 +142,7 @@ object InsightCommentsCrawler {
 					  			CommentsFetcher.readItems(240, 60,topics1h,topics12h,topicsAllTime)
 					  			
 					  			/*Read items published between 4 and 10 hours ago*/
-					  			CommentsFetcher.readItems(6000, 240,topics1h,topics12h,topicsAllTime)
+					  			CommentsFetcher.readItems(600, 240,topics1h,topics12h,topicsAllTime)
 					  			
 					  			/*Wait 20 minutes*/
 					  			Thread.sleep(1200000);
