81bd6e2e20fa6a3fb4a38063171bbf558261bd4a
jb pettit <jbpettit@ebi.ac.uk>
2015-02-03 04:55:26 +0000

    changing database schema again

--- src/main/scala/TopicsFinder.scala
+++ src/main/scala/TopicsFinder.scala
@@ -83,7 +83,7 @@ object TopicsFinder {
 		
 		// filter out words with less than threshold occurrences
 		val filtered = wordCounts.filter((tuple) =>{
-		  (!dictionnary.contains(" "+tuple._1+" ")) && (!dictionnary.contains(" "+tuple._1+"s ")) && tuple._1.length()<15
+		  (!dictionnary.contains(" "+tuple._1+" ")) && (!dictionnary.contains(" "+tuple._1+"s ")) && (!dictionnary.contains(" "+tuple._1+"ed ")) && tuple._1.length()<15
 		})
 		
 		val ret = filtered
