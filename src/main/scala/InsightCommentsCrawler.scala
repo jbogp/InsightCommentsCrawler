8fc26a18a4579162ac1f58af72d340a2b2f01562
jb pettit <jbpettit@ebi.ac.uk>
2015-02-04 16:41:07 +0000

    user profiles

--- src/main/scala/InsightCommentsCrawler.scala
+++ src/main/scala/InsightCommentsCrawler.scala
@@ -138,7 +138,7 @@ object InsightCommentsCrawler {
 								val topicsAllTime = TopicsFinder.getKeywords(100)
 								
 								
-								/*writing time of last computation in mysql
+								/*writing time of last computation in mysql*/
 								val timestamp = Calendar.getInstance().getTimeInMillis()
 								MySQLConnector.connection
 									.createStatement()
@@ -189,7 +189,7 @@ object InsightCommentsCrawler {
 					  			
 					  			/*Read items published between 2 and 4 hours ago*/
 					  			CommentsFetcher.readItems(240, 60,topics1h,topics12h,topicsAllTime)
-								*/
+
 					  			/*Read items published between 4 and 10 hours ago*/
 					  			CommentsFetcher.readItems(600, 240,topics1h,topics12h,topicsAllTime)
 					  			
