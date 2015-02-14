af01c888607ae78e5733932a5eecf6f09521e2dd
jb pettit <jbpettit@ebi.ac.uk>
2015-02-14 03:15:40 +0000

    counting tweets in storm

--- src/main/scala/storm/KafkaStormDemo.scala
+++ src/main/scala/storm/KafkaStormDemo.scala
@@ -137,7 +137,7 @@ class CountBolt extends BaseRichBolt {
 	  
 	  
 	  /*Get the topics*/
-	  val topicsAdded = tuple.getValue(0).asInstanceOf[String]
+	  val topicsAdded = tuple.getString(0)
 	  /*update the count*/
 	  counts.update(topicsAdded, counts(topicsAdded)+1)
   
